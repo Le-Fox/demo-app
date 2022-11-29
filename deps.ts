a6903a0bb4059b902fcbf330d0283a2411929eff
Christoper-96 <smkupa@gicloud.com>
2022-11-29 23:23:05 +0200

    try 5

--- deps.ts
+++ deps.ts
@@ -2,7 +2,7 @@ export * as ReactDOM from "https://jspm.dev/react-dom@17.0.0";
 
 import * as React from "https://jspm.dev/react@17.0.0";
 
-const { default:rest } = React;
+const { default: rest } = React;
 const react = React.default;
 
 export { react as React };
