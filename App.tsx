807f2d713d74d201e659e967a4fbb518fb1dd64c
Christoper-96 <smkupa@gicloud.com>
2022-11-29 23:01:29 +0200

    first commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import {React} from './deps.ts';
+
+function App() {
+  return (
+    <>
+      <link rel={'stylesheet'} href={'./static/css/App.css'}/>
+      <div className="App">
+        <header className="App-header">
+          <img src={'./static/images/logo.svg'} className="App-logo" alt="logo"/>
+          <p>
+            Hi Baby
+          </p>
+          
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
